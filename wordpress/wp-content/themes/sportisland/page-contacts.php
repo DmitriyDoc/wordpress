257d2d1fec7372fa6542c11563ad7e9d992e87cc
Dmitriy <f.doc2@yandex.ru>
2021-02-08 18:53:55 +0300

    Вывод контента на странице контактов. Вывод контента в single.php и реализация вывода даты.

--- wordpress/wp-content/themes/sportisland/page-contacts.php
+++ wordpress/wp-content/themes/sportisland/page-contacts.php
@@ -8,17 +8,16 @@
     <main class="main-content">
     <div class="wrapper">
         <ul class="breadcrumbs">
-            <li class="breadcrumbs__item breadcrumbs__item_home">
-                <a href="index.html" class="breadcrumbs__link">Главная</a>
-            </li>
-            <li class="breadcrumbs__item">
-                <a href="contacts.html" class="breadcrumbs__link">Контакты</a>
-            </li>
+            <?php get_template_part('tmp/breadcrumbs'); ?>
         </ul>
     </div>
     <section class="contacts">
-        <div class="wrapper">
-            <h1 class="contacts__h main-heading">Контакты</h1>
+        <?php
+            while ( have_posts() ) :
+                the_post();
+        ?>
+            <div class="wrapper">
+            <h1 class="contacts__h main-heading"><?php the_title(); ?></h1>
             <div class="map">
                 <a href="#" class="map__fallback">
                     <img src="<?php echo _si_assets_path('/img/map.jpg');?>" alt="Карта клуба SportIsland">
@@ -41,7 +40,7 @@
                     }
                 ?>
             </p>
-            <h2 class="page-heading contacts__h_form"> форма </h2>
+            <?php the_content(); ?>
             <form action="#" class="contacts__form contacts-form">
                 <label class="contacts-form__label">
                     <span class="sr-only"> Имя </span>
@@ -61,7 +60,9 @@
                 </label>
                 <button class="contacts-form__btn btn"> Отправить </button>
             </form>
+
         </div>
+        <?php endwhile; ?>
     </section>
 </main>
 
